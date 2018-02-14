79cdb07c6aa6e41f14506f3638fd3c94369d9476
Sandra Luo <hello@sandraluo.com>
2018-02-13 18:34:06 -0800

    Initial commit

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,48 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Sandra Luo on 2/13/18.
+//  Copyright © 2018 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var categoriesLabel: UILabel!
+    @IBOutlet weak var reviewCountsLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+    var business: Business! {
+        // observer that will update the label whenever it changes
+        didSet {
+            nameLabel.text = business.name
+            thumbImageView.setImageWith(business.imageURL!)
+            categoriesLabel.text = business.categories
+            addressLabel.text = business.address
+            // encapsulating a variable in a string
+            reviewCountsLabel.text = "\(business.reviewCount!) Reviews"
+            distanceLabel.text = business.distance
+        ratingImageView.setImageWith(business.ratingImageURL!)
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // round the image of the business in tableView
+        thumbImageView.layer.cornerRadius = 3
+        thumbImageView.clipsToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Yelp/BusinessesViewController.swift
@@ -0,0 +1,87 @@
+//
+//  BusinessesViewController.swift
+//  Yelp
+//
+//  Created by Timothy Lee on 4/23/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+// implementing protocols for tableView
+class BusinessesViewController: UIViewController, UITableViewDelegate {
+    // need UITableViewDataSource?
+    
+    var businesses: [Business]!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
+            
+            self.businesses = businesses
+            self.tableView.reloadData()
+            
+            if let businesses = businesses {
+                for business in businesses {
+                    print(business.name!)
+                    print(business.address!)
+                }
+            }
+            
+            }
+        )
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            
+            if businesses != nil {
+                return businesses.count
+            }
+            else {
+                return 0
+            }
+        }
+    
+        func tableView(_ tableView: UITableView, cellForRowAtIndexPath: IndexPath) -> UITableViewCell {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
+            
+            // indexPath.row is the index of the cell in the tableView
+            cell.businesses = businesses[indexPath.row]
+            return cell
+            
+        }
+
+        /* Example of Yelp search with more search options specified
+         Business.searchWithTerm("Restaurants", sort: .distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: Error!) -> Void in
+         self.businesses = businesses
+         
+         for business in businesses {
+         print(business.name!)
+         print(business.address!)
+         }
+         }
+         */
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
